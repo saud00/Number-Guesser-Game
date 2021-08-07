fd17b3cad322bc2a9cbdbc58b0c169881598d3fb
Saud <gomail2017@gmail.com>
2021-08-07 23:59:44 +0500

    game functionality added

--- App.js
+++ App.js
@@ -9,18 +9,24 @@ attempts.id = 'attempt' ;
 gameDiv.insertBefore( attempts, input)
 
 
-let inputVal = input.addEventListener('input', function(e){
-    return e.target.value
-})
+let inputVal;
+input.addEventListener('input', inputFun); 
+function inputFun(e){
+    inputVal = parseInt(e.target.value);
+}
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8 , 9 , 10];
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8 , 9 , 10];
 
 const min = Math.min(...arr);
 const max = Math.max(...arr);
 
-let pivet = document.addEventListener('DOMContentLoaded', function (){
-    return Math.random(...arr)
-})
+let pivet;
+document.addEventListener('DOMContentLoaded', random) 
+
+function random(){
+    pivet = Math.floor(Math.random() * 11)
+}
 
 document.getElementById('min-num').textContent = min;
 document.getElementById('max-num').textContent = max;
@@ -28,6 +34,7 @@ document.getElementById('max-num').textContent = max;
 submit.addEventListener('click', submitFun);
 
 let i = 1; attempt , end = 3; 
+
 function submitFun(e){
     e.preventDefault();
     console.log(pivet, inputVal)
@@ -36,19 +43,25 @@ function submitFun(e){
             input.style.borderColor = "green";
             msg.textContent = "Yahoo0 ! You cracked"
             msg.style.color = "green"
-            input.setAttribute('disabled', true)
+            input.setAttribute('disabled', "")
             i=3;
         }else{
             msg.textContent = "Try Again"
             msg.style.color = "red";
             attempt = end - i;
-            attempts.textContent = `You have ${attempt} attempts left `
+            attempts.textContent = `You have ${attempt} attempts left `;
             i++;
+            console.log(attempt, i );
+            input.focus();
+            input.value = null
+            input.placeholder = `Enter ${attempt} value`;
+            inputFun();
         }
         
     }while(i < end)
     
-    input.setAttribute('disabled')
+    console.log(attempt,"after",i );
+    input.setAttribute('disabled',"")
     
 }
 
